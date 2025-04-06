9a9a61ad64fd2bb0054274474e4ca897d4c4443f
Alexis Graff <alexisgraff123@gmail.com>
2025-04-06 00:04:07 -0300

    fix: lint

--- src/Shared/Infrastructure/DatabaseConnection.ts
+++ src/Shared/Infrastructure/DatabaseConnection.ts
@@ -2,10 +2,10 @@ import { DataSource } from 'typeorm';
 
 import { PermissionEntity } from '../../Auth/Infrastructure/schemas/PermissionSchema';
 import { RoleEntity } from '../../Auth/Infrastructure/schemas/RoleSchema';
+import { UserPermissionEntity } from '../../Auth/Infrastructure/schemas/UserPermissionSchema';
 import { UserRoleEntity } from '../../Auth/Infrastructure/schemas/UserRoleSchema';
 import { UserEntity } from '../../Auth/Infrastructure/schemas/UserSchema';
 import { EnvService } from '../../Config/Env/EnvService';
-import { UserPermissionEntity } from '../../Auth/Infrastructure/schemas/UserPermissionSchema';
 
 export const DatabaseConnections = [
   {
