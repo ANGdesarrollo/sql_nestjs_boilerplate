e14680392922b325ae0f169e6319953c9cf28c00
Alexis Graff <alexisgraff123@gmail.com>
2025-04-06 18:30:01 -0300

    feat: update login jwt

--- src/Auth/Application/LoginUserUseCase.ts
+++ src/Auth/Application/LoginUserUseCase.ts
@@ -4,28 +4,62 @@ import { JwtService } from '@nestjs/jwt';
 import { LoginUserPayload } from '../Domain/Payloads/LoginUserPayload';
 import { HashService } from '../Domain/Services/HashService';
 import { UserRepository } from '../Infrastructure/repositories/UserRepository';
+import { UserRoleRepository } from '../Infrastructure/repositories/UserRoleRepository';
+import { UserTenantRepository } from '../Infrastructure/repositories/UserTenantRepository';
 
-@Injectable ()
+interface JwtPayload {
+  username: string;
+  userId: string;
+  tenantId: string;
+  permissions: string[];
+}
+
+@Injectable()
 export class LoginUserUseCase
 {
   constructor(
-    private readonly repository: UserRepository,
-    private readonly  jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+    private readonly userRoleRepository: UserRoleRepository,
+    private readonly userTenantRepository: UserTenantRepository,
+    private readonly jwtService: JwtService,
     private readonly hashService: HashService
   ) {}
 
   async execute(body: LoginUserPayload)
   {
     const user = await this.findUser(body.username);
-
     await this.comparePassword(user.password, body.password);
 
-    return this.jwtService.sign(body);
+    const defaultUserTenant = await this.userTenantRepository.getDefaultTenant(user.id);
+    if (!defaultUserTenant)
+    {
+      throw new UnauthorizedException('User does not have a default tenant');
+    }
+
+    const userRoles = await this.userRoleRepository.getUserRoles(user.id);
+    const permissions = new Set<string>();
+
+    userRoles.forEach(userRole =>
+    {
+      userRole.role.permissions.forEach(permission =>
+      {
+        permissions.add(permission.name);
+      });
+    });
+
+    const payload: JwtPayload = {
+      username: user.username,
+      userId: user.id,
+      tenantId: defaultUserTenant.tenant.id,
+      permissions: Array.from(permissions)
+    };
+
+    return this.jwtService.sign(payload);
   }
 
   private async findUser(username: string)
   {
-    const user = await this.repository.findOneBy('username', username);
+    const user = await this.userRepository.findOneBy('username', username);
     if (!user)
     {
       throw new UnauthorizedException('User or password incorrect');
--- src/Auth/Infrastructure/repositories/UserRoleRepository.ts
+++ src/Auth/Infrastructure/repositories/UserRoleRepository.ts
@@ -4,15 +4,31 @@ import { Repository } from 'typeorm';
 import { BaseTypeOrmRepositoryImpl } from '../../../Shared/Infrastructure/BaseTypeOrmRepositoryImpl';
 import { UserRoleDomain } from '../../Domain/Entities/UserRoleDomain';
 import { UserRolePayload } from '../../Domain/Payloads/UserRolePayload';
+import { UserRoleEntity } from '../schemas/UserRoleSchema';
 
 @Injectable()
 export class UserRoleRepository extends BaseTypeOrmRepositoryImpl<UserRolePayload, UserRoleDomain>
 {
   constructor(
     @Inject('USER_ROLE_REPOSITORY')
-      userRoleRepository: Repository<UserRoleDomain>
+      userRoleRepository: Repository<UserRoleEntity>
   )
   {
     super(userRoleRepository, 'UserRoleEntity');
+  }
+
+  async getUserRoles(userId: string): Promise<UserRoleDomain[]>
+  {
+    try
+    {
+      return await this.repository.find({
+        where: { user: { id: userId } },
+        relations: ['role', 'role.permissions']
+      });
+    }
+    catch (error)
+    {
+      this.handleTypeOrmError(error, 'getUserRoles');
+    }
   }
 }
