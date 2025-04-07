5f9bcdded5331d25eb093771382a22a96b7cc076
Alexis Graff <alexisgraff123@gmail.com>
2025-04-07 20:50:08 -0300

    feat: config tests

--- src/Config/Env/EnvConfig.ts
+++ src/Config/Env/EnvConfig.ts
@@ -11,7 +11,11 @@ export const envConfig = {
   DATABASE_USER: Joi.string().default('postgres'),
   DATABASE_PASSWORD: Joi.string().required(),
   DATABASE_NAME: Joi.string().default('nest_auth'),
-
+  TEST_DATABASE_HOST: Joi.string().default('localhost'),
+  TEST_DATABASE_PORT: Joi.number().default(5433),
+  TEST_DATABASE_USER: Joi.string().default('postgres'),
+  TEST_DATABASE_PASSWORD: Joi.string().default('postgres'),
+  TEST_DATABASE_NAME: Joi.string().default('nest_auth_test'),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRES_IN: Joi.string().default('1h'),
 
