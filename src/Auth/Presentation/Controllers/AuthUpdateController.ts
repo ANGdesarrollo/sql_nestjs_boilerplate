15a427093796efc67a415923ff75e3402d583f01
Alexis Graff <alexisgraff123@gmail.com>
2025-04-07 20:15:47 -0300

    feat: add readme

--- src/Auth/Presentation/Controllers/AuthUpdateController.ts
+++ src/Auth/Presentation/Controllers/AuthUpdateController.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Param, Put, UseGuards } from '@nestjs/common';
+
+import { Permissions } from '../../../Config/Permissions';
+import { UpdateTenantUseCase } from '../../Application/UpdateTenantUseCase';
+import { TenantPayload } from '../../Domain/Payloads/TenantPayload';
+import { RequirePermissions } from '../Decorators/RequirePermissions';
+import { AuthGuard } from '../Guards/AuthGuard';
 
 @Controller('auth')
-export class AuthUpdateController {}
+export class AuthUpdateController
+{
+  constructor(
+    private readonly updateTenantUseCase: UpdateTenantUseCase
+  ) {}
+
+  @Put('tenant/:id')
+  @UseGuards(AuthGuard)
+  @RequirePermissions(Permissions.TENANT.UPDATE)
+  async updateTenant(
+    @Param('id') id: string,
+    @Body() body: Partial<TenantPayload>
+  )
+  {
+    return this.updateTenantUseCase.execute({ id, data: body });
+  }
+}
