9b7dd2542b16e42d091837bc8ac45a6aae59d9a3
Alexis Graff <alexisgraff123@gmail.com>
2025-04-07 23:13:19 -0300

    feat: login test

--- test/Auth/login-user-usecase.test.ts
+++ test/Auth/login-user-usecase.test.ts
@@ -1,10 +1,29 @@
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
+import { LoginUserUseCase } from '../../src/Auth/Application/LoginUserUseCase';
 import { getTestAgent } from '../TestAgent';
 
 let app: NestFastifyApplication;
+let loginUserUseCase: LoginUserUseCase;
 
 beforeAll(async() =>
 {
   app = await getTestAgent();
+  loginUserUseCase = app.get(LoginUserUseCase);
+});
+
+describe('LoginUserUseCase', () =>
+{
+  describe('execute', () =>
+  {
+    it('should return a JWT token', async() =>
+    {
+      const result = await loginUserUseCase.execute({
+        username: 'admin',
+        password: 'admin'
+      });
+
+      expect(result).toBeDefined();
+    });
+  });
 });
