c325521c258fb8ec2015b07cec016db19ee48976
Alexis <alexisgraff123@gmail.com>
2025-05-07 20:38:50 -0300

    feat: add vencimiento

--- src/File/Application/GetFileUseCase.ts
+++ src/File/Application/GetFileUseCase.ts
@@ -1,16 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
+import { Logger } from '../../Shared/Presentation/Utils/Logger';
 import { FileDomain } from '../Domain/Entities/FileDomain';
+import { MinioService } from '../Domain/Services/MinioService';
 import { FileRepository } from '../Infrastructure/Repositories/FileRepository';
+
+interface FileWithUrl extends FileDomain {
+  url?: string;
+}
 
 @Injectable()
 export class GetFileUseCase
 {
   constructor(
-    private readonly fileRepository: FileRepository
+    private readonly fileRepository: FileRepository,
+    private readonly minioService: MinioService
   ) {}
 
-  async execute(fileId: string): Promise<FileDomain>
+  async execute(fileId: string): Promise<FileWithUrl>
   {
     const file = await this.fileRepository.findOneBy('id', fileId);
 
@@ -19,6 +26,16 @@ export class GetFileUseCase
       throw new NotFoundException(`File with ID ${fileId} not found`);
     }
 
-    return file;
+
+    try
+    {
+      const url = await this.minioService.getFileUrl(file.path, file.isPublic, 3600);
+      return { ...file, path: url };
+    }
+    catch (error)
+    {
+      Logger.error(`Failed to generate URL for file ${fileId}:`, error.message);
+      return file;
+    }
   }
 }
