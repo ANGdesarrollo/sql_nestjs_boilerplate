bb557ad39d12f0bf5a48f451a3ce5fee412f5a1b
Alexis <alexisgraff123@gmail.com>
2025-05-29 23:29:05 -0300

    feat: change uuid for id incremental

--- src/File/Application/index.ts
+++
@@ -1,9 +0,0 @@
-import { DownloadFileUseCase } from './DownloadFileUseCase';
-import { GetFileUseCase } from './GetFileUseCase';
-import { UploadFileUseCase } from './UploadFileUseCase';
-
-export const FileUseCases = [
-  UploadFileUseCase,
-  DownloadFileUseCase,
-  GetFileUseCase
-];
--- src/File/Presentation/Controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { FileGetController } from './FileGetController';
-import { FilePostController } from './FilePostController';
-
-export const FileControllers = [
-  FileGetController,
-  FilePostController
-];
